77439c6eafeaa4f30082ee93e44df245b85fea6c
Draiker <daniel.vz.gc97@gmail.com>
2020-12-19 18:16:54 -0600

    Inicio

---
+++ 1_Sintais/Script.js
@@ -0,0 +1,32 @@
+const z=10; //declaracion de una constante
+
+let x=0; //variable de tipo entero
+let a='l'; //variable de tipo caracter
+let b='Hola';//variable de tipo String
+
+let i=true;   //declaracion de variables
+let j=false;  //booleanas
+
+x=2; //Cambiar el valor de una variable
+console.log(z, x, a, b, i, j); // imprimir en consola del buscador
+//---Operadores Matematicos---
+console.log(x+z);
+console.log(z-x);
+console.log(x*z);
+console.log(x/z);
+console.log(z%x);
+//---Operadores De Asigncion---
+console.log(x+=z);//x=x+z
+console.log(x-=z);//x=x-z
+console.log(x*=z);//x=x*z
+console.log(x/=z);//x=x/z
+console.log(x%=2);//x=x%2
+//---Operadores De Incremento y Decremeno---
+console.log(x++);
+console.log(++x);
+console.log(x--);
+console.log(--x);
+//---concatenacion---
+console.log(b + " Draiker"); //+ es usado para concatenar
+//---concatenacion y suma---
+console.log("x+z= "+(x+z));
